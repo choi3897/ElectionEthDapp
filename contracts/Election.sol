66b59b7c754005596f4bb3a727107abeee7f07f9
choi3897 <eois@naver.com>
2019-01-22 00:04:08 +0900

    Ethereum Dapp Tutorial - Election App (step till 2. List Candidates)

---
+++ contracts/Election.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.5.0;
+
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Read/write candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+}
