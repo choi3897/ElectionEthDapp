1b626f7f8c25c144b962810e336622309683a9d2
choi3897 <eois@naver.com>
2019-01-22 23:50:12 +0900

    Step3. Cast Votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -9,11 +9,13 @@ contract Election {
         uint voteCount;
     }
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
     // Read/write candidates
     mapping(uint => Candidate) public candidates;
-
     // Store Candidates Count
     uint public candidatesCount;
+
     constructor() public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
@@ -22,6 +24,20 @@ contract Election {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender],"This Voter has already voted!");
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "There is no such candidate");
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidatte vote Count
+        candidates[_candidateId].voteCount++;
     }
 
 }
